523092dd564152c4eec4be164c75994e3d983b38
emmanuella <237996564+qella822-web@users.noreply.github.com>
2026-06-08 18:24:04 -0700

    Added TableAndChairs and Triangle assignments

---
+++ src/TableAndChairs.java
@@ -0,0 +1,23 @@
+public class TableAndChairs {
+
+        public static void main(String[] args){
+
+       System.out.println("X                                            X");
+       System.out.println("X                                            X");
+       System.out.println("X                                            X");
+       System.out.println("XXXX             XXXXXXXXXXXXXX           XXXX");
+       System.out.println("X  X             X            X           X  X");
+       System.out.println("X  X             X            X           X  X");
+       System.out.println("X  X             X            X           X  X");
+       System.out.println("X  X             X            X           X  X");
+
+
+
+    }
+
+
+
+
+
+
+}
